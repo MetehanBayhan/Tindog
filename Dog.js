f53d9482d271e966940abe5dfcd29dd5c035cc1a
MetehanBayhan <metehnb@gmail.com>
2023-04-21 00:24:49 +0300

    asdasd

--- Dog.js
+++ Dog.js
@@ -1,10 +1,24 @@
 // Create the Dog class here
-import dogs from "./data"
+import dogs from "./data.js"
 
 class Dog {
   constructor(data) {
     Object.assign(this, data)
   }
+
+  getDogHtml() {
+    const {avatar, name, age, bio} = this
+    return `
+    <img src="./${avatar}" alt="">
+    <div class="gradient-overlay"></div>
+    <p class="liked">LIKE</p>
+    <div class="image-info-wrapper">
+        <h2 class="user-info">${name}, ${age}</h2>
+        <p class="user-bio">${bio}</p>
+    </div>
+    `
+  }
+
 }
 
 
--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 // Remember to import the data and Dog class!
+import dogs from "./data.js"
+import Dog from "./Dog.js"
 
-import dogs from "./data"
-import dog from "./Dog"
+
+function getNexDog() {
+  const nextDogData = dogs.shift()
+  return nextDogData ? new Dog(nextDogData) : {}
+}
+
+function render() {
+  document.querySelector(".image-wrapper").innerHTML = currentDog.getDogHtml()
+}
+
+
+
+
+const likeBtn = document.querySelector(".like-btn")
+const passBtn = document.querySelector(".pass-btn")
+
+likeBtn.addEventListener('click', function() {
+  currentDog.hasBeenLiked = true
+  currentDog.hasBeenSwiped = true
+  console.log(currentDog)
+  currentDog = getNexDog()
+  render()
+});
+
+passBtn.addEventListener('click', function() {
+  currentDog.hasBeenSwiped = true
+  console.log(currentDog)
+  currentDog = getNexDog()
+  render()
+});
+
+
+let currentDog = getNexDog()
+render()
