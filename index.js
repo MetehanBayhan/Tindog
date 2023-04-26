0d2793553b3e1153268f882d3ad63a176b404d78
MetehanBayhan <metehnb@gmail.com>
2023-04-26 18:14:09 +0300

    last touches

--- index.js
+++ index.js
@@ -10,19 +10,23 @@ function getNexDog() {
 
 let currentDog = {}
 function render() {
-  console.log("#1");
-  currentDog = getNexDog()
-  document.querySelector(".image-wrapper").innerHTML = currentDog.getDogHtml()
-  
+  if(dogs.length > 0 ){
+    enableSwipeButtons()
+    currentDog = getNexDog()
+    document.querySelector(".image-wrapper").innerHTML = currentDog.getDogHtml()
+  }else{
+    console.log("data is empty");
+    disableSwipeButtons()
+  }
 }
 
 // DECLARE BUTTONS
 const likeBtn = document.querySelector(".like-btn")
 const passBtn = document.querySelector(".pass-btn")
 
-
 // BUTTON LISTENERS
 likeBtn.addEventListener('click', () => {
+  disableSwipeButtons()
   document.querySelector(".image-wrapper").innerHTML += `<img src="./images/badge-like.png" alt="green liked badge" class="badge">`
   currentDog.hasBeenLiked = true
   currentDog.hasBeenSwiped = true
@@ -30,16 +34,21 @@ likeBtn.addEventListener('click', () => {
 });
 
 passBtn.addEventListener('click', () => {
+  disableSwipeButtons()
   document.querySelector(".image-wrapper").innerHTML += `<img src="./images/badge-nope.png" alt="red nope badge" class="badge">`
   currentDog.hasBeenSwiped = true
   setTimeout(render, 2000)
 });
 
 
-function disableButtons() {
-  likeBtn.disable = true
-  passBtn.disable = true
+function disableSwipeButtons() {
+  likeBtn.disabled = true
+  passBtn.disabled = true
 }
 
+function enableSwipeButtons() {
+  likeBtn.disabled = false
+  passBtn.disabled = false
+}
 
 render()
